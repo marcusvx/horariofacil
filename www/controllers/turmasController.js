7e0a5b8e25b15a83f44cdd163a462b338b070331
marcusvx <marcus_vx@yahoo.com.br>
2015-06-02 23:49:21 -0300

    Utilização de código de script carregado dinamicamente
    Solução referente a http://pt.stackoverflow.com/questions/67226/como-compartilhar-fun%c3%a7%c3%b5es-entre-arquivos-javascript/67244#comment137432_67244

--- www/controllers/turmasController.js
+++ www/controllers/turmasController.js
@@ -1,32 +1,28 @@
 // Objeto controller para guardar as funções necessárias.
 var controller = {
 
-	// Obtendo o nome da turma selecionado pelo usuário.
-	setNomeTurma : function(turma) {
-		if (turma != null) {
-			location.href = "semestres.html?" + turma;
-		} else {
-			alert("Ops. Ocorreu um erro, vamos tentar de novo!");
-			location.href = "turmas.html";
-		}
-	},
+    // Obtendo o nome da turma selecionado pelo usuário.
+    setNomeTurma: function (turma) {
+        if (turma != null) {
+            location.href = "semestres.html?" + turma;
+        } else {
+            alert("Ops. Ocorreu um erro, vamos tentar de novo!");
+            location.href = "turmas.html";
+        }
+    },
 
-	// Obtendo todas as turmas para serem exibidas na tela.
-	getTurmas : function() {
+    // Obtendo todas as turmas para serem exibidas na tela.
+    getTurmas: function () {
+        function carregarTurmas() {
+            var resultado = [];
+            var turmas = new turmasDao();
+            resultado = turmas.getTurmas();
+            if (resultado.length > 0) {
+                alert(resultado[0].Name);
+            }
+        }
 
-		var resultado = [];
-
-		loadDependence("Dao/turmasDao.js", function() {
-			var turmas = new turmasDao();
-			resultado = turmas.getTurmas();
-
-		});
-		
-		if(resultado.length > 0){
-			alert(resultado[0].Name);
-		}
-		
-
-	}
+        loadDependence("Dao/turmasDao.js", carregarTurmas);
+    }
 
 };
